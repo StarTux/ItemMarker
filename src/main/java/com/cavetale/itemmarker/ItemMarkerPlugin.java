bf3b09d4afae280cb45ecf51d8118eb8b41a7194
Thomas Loy <ths.loy@googlemail.com>
2019-07-31 16:11:41 +0200

    Custom event

--- src/main/java/com/cavetale/itemmarker/ItemMarkerPlugin.java
+++ src/main/java/com/cavetale/itemmarker/ItemMarkerPlugin.java
@@ -3,17 +3,13 @@ package com.cavetale.itemmarker;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ItemMarkerPlugin extends JavaPlugin {
-    private ItemListener itemListener;
     private ItemMarkerCommand itemMarkerCommand;
 
     @Override
     public void onEnable() {
-        this.itemListener = new ItemListener(this);
         this.itemMarkerCommand = new ItemMarkerCommand(this);
         getCommand("itemmarker").setExecutor(this.itemMarkerCommand);
-    }
-
-    @Override
-    public void onDisable() {
+        getServer().getPluginManager()
+            .registerEvents(new EventListener(), this);
     }
 }
